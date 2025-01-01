97a2cec00b62e0c7517d1ef2ee82d58bd557148b
aepii <simentalma@yahoo.com>
2025-01-01 15:21:01 -0600

    Python insertion sort implementation with ascending and descending order support.

---
+++ src/python/sorting_algorithms/main.py
@@ -0,0 +1,10 @@
+from insertion_sort import *
+
+def main():
+    my_array = [5, 2, 4, 6, 1, 3]
+    print(f"Before Insertion Sort: {my_array}")
+    insertion_sort(my_array)
+    print(f"After Insertion Sort: {my_array}")
+
+if __name__ == "__main__":
+    main()
