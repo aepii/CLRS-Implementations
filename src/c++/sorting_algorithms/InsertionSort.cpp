1366c8479c7a73659266bd79a4ecf781fdf4d6c2
aepii <simentalma@yahoo.com>
2025-01-01 16:23:36 -0600

    Implemented insertion sort algorithm in c++.

--- src/c++/sorting_algorithms/InsertionSort.cpp
+++ src/c++/sorting_algorithms/InsertionSort.cpp
@@ -4,4 +4,14 @@
 #include <vector>
 
 void insertionSort(std::vector<int>& array){
+  for(size_t j = 1; j < array.size(); ++j){
+    int key = array[j];
+    int i = j - 1;
+
+    while(j >= 0 && array[i] > key){
+      array[i + 1] = array[i];
+      i -= 1;
+    }
+    array[i + 1] = key;
+  }
 }
