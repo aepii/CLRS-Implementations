e416de93e833b0c904534d66660cc74baac6c795
aepii <simentalma@yahoo.com>
2025-01-02 19:14:13 -0600

    Added explanation on merge sort.

--- src/py/merge_sort/merge_sort.py
+++ src/py/merge_sort/merge_sort.py
@@ -1,26 +1,52 @@
 import math
 
+"""
+Sorting algorithm which utilizes the divide-and-conquer paradigm by
+dividing an array into subarrays while recursively sorting each subarray.
+"""
 def merge_sort(array, p, r):
-    if p < r:
-        q = (p + r) // 2
-        merge_sort(array, p, q) 
-        merge_sort(array, q + 1, r)
-        merge(array, p, q, r) 
+    # Divide the array if the left index is smaller than the right index.
+    if p < r: 
+        q = (p + r) // 2 # Compute the midpoint of the array.
+        merge_sort(array, p, q) # Divide into a left subarray.
+        merge_sort(array, q + 1, r) # Divide into a right subarray.
+        merge(array, p, q, r) # Sort the contents of the subarray.
 
 def merge(array, p, q, r):
-    n1 = q - p + 1
-    n2 = r - q
+    n1 = q - p + 1 # Compute the size of the left subarray.
+    n2 = r - q # Compute the size of the right subarray.
 
-    left_array = array[p:q+1] + [math.inf]
-    right_array = array[q+1:r+1] + [math.inf]
+    left_array = array[p:q+1] + [math.inf] # Copy the left half of the array into a left subarray plus a sentinel value.
+    right_array = array[q+1:r+1] + [math.inf] # Copy the right half of the array into a right subarray plus a sentinel value.
 
-    i = j = 0
+    # The sentinel values ensure the indices, i and j, do not
+    # go out of bounds which gaurantees all elements to be exhausted.
 
+    i = j = 0 # Initialize indices, i and j, to zero.
+    
+    # Iterate through all elements from the left to the right subarrays.
     for k in range(p, r + 1):
-        print(left_array)
-        if left_array[i] <= right_array[j]:
-            array[k] = left_array[i]
-            i += 1
+        # If the element from the left array is smaller than the element from the right array.
+        if left_array[i] <= right_array[j]: 
+            array[k] = left_array[i] # Place the element from the left array into its correct position.
+            i += 1 # Increment the left array's pointer.
+        # Else, the element from the right array must be smaller than the element from the left array.
         else:
-            array[k] = right_array[j]
-            j += 1
+            array[k] = right_array[j] # Place the element from the right array into its correct position.
+            j += 1 # Increment the right array's pointer.
+
+"""
+To sort in descending order, simply negate the sentinel values:
+
+left_array = array[p:q+1] + [-math.inf]
+right_array = array[q+1:r+1] + [-math.inf]
+
+additionally, the comparison logic should be flipped:
+
+if left_array[i] > right_array[j]:
+    array[k] = left_array[i]
+    i += 1
+else:
+    array[k] = right_array[j]
+    j += 1
+"""
