cc2962c0579413366965b954ca6cc40ae42a6eef
aepii <simentalma@yahoo.com>
2025-01-03 17:44:02 -0600

    Fixed JS merge sort implementation.

--- src/js/merge_sort/main.js
+++ src/js/merge_sort/main.js
@@ -3,7 +3,7 @@ import { mergeSort } from "./merge-sort.js";
 function main() {
   const myArray = [5, 2, 4, 6, 1, 3];
   console.log(`Before Insertion Sort: ${myArray}`);
-  mergeSort(myArray);
+  mergeSort(myArray, 0, myArray.length - 1);
   console.log(`After Insertion Sort: ${myArray}`);
 }
 
